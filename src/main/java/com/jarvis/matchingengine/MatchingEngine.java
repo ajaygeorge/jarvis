774457cf04c5e5a0ebf9619204066a1db4931869
Ajay George <ajay.george@outlook.com>
2013-10-21 19:28:29 -0700

    Added looping in MatchingEngine

--- src/main/java/com/jarvis/matchingengine/MatchingEngine.java
+++ src/main/java/com/jarvis/matchingengine/MatchingEngine.java
@@ -16,27 +16,29 @@ public class MatchingEngine {
 	public void match(OrderBook orderBook) {
 		SortedSet<Order> buyQueue = orderBook.getBuyQueue();
 		SortedSet<Order> sellQueue = orderBook.getSellQueue();
-		if (buyQueue.size()!=0 && sellQueue.size()!=0) {
+		while (buyQueue.size()!=0 && sellQueue.size()!=0) {
 			Order buy = buyQueue.last();
 			Order sell = sellQueue.last();
 			int priceComparator = buy.getPrice().compareTo(sell.getPrice());
 			int volumeComparator = buy.getVolume().compareTo(sell.getVolume());
 			if (priceComparator >= 0 && volumeComparator == 0) {
-				System.out.println("Order matched at " + sell.getPrice());
+				//System.out.println("Order matched at " + sell.getPrice());
 				buyQueue.remove(buy);
 				sellQueue.remove(sell);
 			} else if (priceComparator >=0 && volumeComparator > 0) {
-				System.out.println("Order matched at " + sell.getPrice());
-				System.out.println("Partial fill for " + sell.getVolume());
+				//System.out.println("Order matched at " + sell.getPrice());
+				//System.out.println("Partial fill for " + sell.getVolume().divide(BigInteger.valueOf(1000*1000)));
 				BigInteger currBuyVolume = buy.getVolume();
 				buy.setVolume(currBuyVolume.subtract(sell.getVolume()));
 				sellQueue.remove(sell);
 			} else if (priceComparator >=0 && volumeComparator < 0) {
-				System.out.println("Order matched at " + sell.getPrice());
-				System.out.println("Partial fill for " + buy.getVolume());
+				//System.out.println("Order matched at " + sell.getPrice());
+				//System.out.println("Partial fill for " + buy.getVolume().divide(BigInteger.valueOf(1000*1000)));
 				BigInteger currSellVolume = sell.getVolume();
 				sell.setVolume(currSellVolume.subtract(buy.getVolume()));
 				buyQueue.remove(buy);
+			} else if (priceComparator <0) {
+				return;
 			}
 		}
 	}
